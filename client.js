f2f6440f91bc7b4753f895b61a610f7a8d30af74
merly267 <emily.harding.2015@live.rhul.ac.uk>
2022-12-27 21:22:42 +0000

    Use env variables

--- client.js
+++ client.js
@@ -1,8 +1,10 @@
 import sanityClient from "@sanity/client";
 
-export default sanityClient({
-  projectId: "qg5y6uny", // you can find this in sanity.config.ts (sanity.json prior to v3)
-  dataset: "production",
-  apiVersion: "2022-12-27", // use current UTC date - see http://sanity.io/docs/api-versioning
-  useCdn: true, // `false` if you want to ensure fresh data ie for ISR
-});
+const client = sanityClient({
+    projectId: process.env.SANITY_PROJECT_ID, // you can find this in sanity.config.ts (sanity.json prior to v3)
+    dataset: process.env.SANITY_DATASET,
+    apiVersion: "2022-12-27", // use current UTC date - see http://sanity.io/docs/api-versioning
+    useCdn: true, // `false` if you want to ensure fresh data ie for ISR
+  });
+
+  export default client
