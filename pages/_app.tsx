b3cce799fb766441ee3cb22b7bb11383f9ba0abb
merly267 <emily.harding.2015@live.rhul.ac.uk>
2023-01-05 22:46:47 +0000

    Add plausible.io

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
+import PlausibleProvider from 'next-plausible'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <PlausibleProvider domain="incipitsatura.com">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </PlausibleProvider>
   )
 }
