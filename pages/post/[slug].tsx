f89f053dd62098e570e5cd5982070fd39e7e4568
merly267 <emily.harding.2015@live.rhul.ac.uk>
2023-03-09 23:01:11 +0000

    convert slug to TS

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,18 +1,26 @@
 import client from '../../client'
 import Seo from '../../components/Seo'
 import BlockContent from '../../components/BlockContent'
+import { InferGetStaticPropsType } from 'next'
 
 import styles from '../../styles/post.module.css'
 
-const Post = ({ post, currentSlug }) => {
+type PostData = {
+    title: string
+    description: string
+    publishedAt: Date
+    body: object[];
+}
+
+const Post = ({ post, currentSlug }: InferGetStaticPropsType<typeof getStaticProps>) => {
     if (!post) {
         return null
     }
     return (
         <article>
             <Seo
-                title={post?.title}
-                description={post?.description}
+                title={post?.title ? post.title : process.env.NEXT_PUBLIC_SITE_TITLE}
+                description={post?.description ? post.description : process.env.NEXT_PUBLIC_SITE_DESCRIPTION}
                 url={`${process.env.NEXT_PUBLIC_SITE_ROOT}/post/${currentSlug}`}
             />
             <h1 className={styles.heading}>{post?.title}</h1>
@@ -29,13 +37,13 @@ export async function getStaticPaths() {
         `*[_type == "post" && defined(slug.current)][].slug.current`
     )
     return {
-        paths: paths.map((slug) => ({ params: { slug } })),
+        paths: paths.map((slug: string) => ({ params: { slug } })),
         fallback: false,
     }
 }
 
-export async function getStaticProps({ params }) {
-    const post = await client.fetch(
+export async function getStaticProps({ params }: any) {
+    const post: PostData = await client.fetch(
         `*[_type == "post" && slug.current == "${params.slug}"][0]{
             title,
             description,
